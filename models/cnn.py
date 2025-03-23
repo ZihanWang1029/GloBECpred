961e4e55ff26560c1892cd87df0bec6edad9f540
ZihanWang1029 <167680315+ZihanWang1029@users.noreply.github.com>
2025-03-23 19:32:55 +0800

    Add files via upload

---
+++ models/cnn.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+class CNNModel(nn.Module):
+    def __init__(self, output_dim=10):
+        super(CNNModel, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm1d(32)
+        self.pool1 = nn.MaxPool1d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv1d(32, 64, 3, padding=1)
+        self.bn2 = nn.BatchNorm1d(64)
+        self.pool2 = nn.MaxPool1d(2, 2)
+
+        self.conv3 = nn.Conv1d(64, 128, 3, padding=1)
+        self.bn3 = nn.BatchNorm1d(128)
+        self.pool3 = nn.MaxPool1d(2, 2)
+
+        self.flatten_dim = 128 * 21
+
+        self.fc1 = nn.Linear(self.flatten_dim, 256)
+        self.bn_fc1 = nn.BatchNorm1d(256)
+        self.dropout1 = nn.Dropout(0.5)
+
+        self.fc2 = nn.Linear(256, 128)
+        self.bn_fc2 = nn.BatchNorm1d(128)
+        self.dropout2 = nn.Dropout(0.5)
+
+        self.fc3 = nn.Linear(128, output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.pool1(self.relu(self.bn1(self.conv1(x))))
+        x = self.pool2(self.relu(self.bn2(self.conv2(x))))
+        x = self.pool3(self.relu(self.bn3(self.conv3(x))))
+        x = x.view(x.size(0), -1)
+        x = self.dropout1(self.relu(self.bn_fc1(self.fc1(x))))
+        x = self.dropout2(self.relu(self.bn_fc2(self.fc2(x))))
+        x = self.fc3(x)
+        return x
